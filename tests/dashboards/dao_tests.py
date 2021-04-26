115edcc924a47ae32f8c25c26c6efa4d3028a6ab
erik_ritter <erik.ritter@airbnb.com>
2021-04-26 10:55:49 -0700

    feat: Add etag caching to dashboard APIs

--- tests/dashboards/dao_tests.py
+++ tests/dashboards/dao_tests.py
@@ -79,3 +79,30 @@ class TestDashboardDAO(SupersetTestCase):
 
         # reset dash to original data
         DashboardDAO.set_dash_metadata(dash, original_data)
+
+    @pytest.mark.usefixtures("load_world_bank_dashboard_with_slices")
+    def test_get_dashboard_changed_on(self):
+        session = db.session()
+        dashboard = session.query(Dashboard).filter_by(slug="world_health").first()
+
+        assert dashboard.changed_on == DashboardDAO.get_dashboard_changed_on(dashboard)
+        assert dashboard.changed_on == DashboardDAO.get_dashboard_changed_on(
+            "world_health"
+        )
+
+        old_changed_on = dashboard.changed_on
+
+        data = dashboard.data
+        positions = data["position_json"]
+        data.update({"positions": positions})
+        original_data = copy.deepcopy(data)
+
+        data.update({"foo": "bar"})
+        DashboardDAO.set_dash_metadata(dashboard, data)
+        session.merge(dashboard)
+        session.commit()
+        assert old_changed_on < DashboardDAO.get_dashboard_changed_on(dashboard)
+
+        DashboardDAO.set_dash_metadata(dashboard, original_data)
+        session.merge(dashboard)
+        session.commit()
